4a18af8a11f16528a6ea36279208980aa974e041
Giuseppe Senettone <92723630+giusene@users.noreply.github.com>
2022-02-08 13:35:50 +0100

    Bootstrap/routing and redux (#15)
    * feat: #4 add routing and redux
    
    * fix: fix deploy problems

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.scss';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { BrowserRouter as Router } from 'react-router-dom'
 import { store } from './store/loginStore';
 import { Provider } from 'react-redux';
 
