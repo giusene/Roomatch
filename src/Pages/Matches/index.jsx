84e53da60a4ce82383e6a1dde8d1efbe5a9ebcaf
Rebecca Mollica <rebecca.mollica@live.it>
2022-02-13 10:48:56 +0100

    feat: add matches component (#64)

--- src/Pages/Matches/index.jsx
+++ src/Pages/Matches/index.jsx
@@ -1,9 +1,11 @@
+import MatchesProfiles from '../../components/MatchesProfiles'
 import styles from './Matches.module.scss'
 
 const Matches = () => {
     return (
         <div className={styles.main}>
             <h4>Matches Page</h4>
+            <MatchesProfiles />
         </div>
     )
 } 
---
+++ src/components/MatchesProfiles/index.jsx
@@ -0,0 +1,82 @@
+import styles from "./MatchesProfiles.module.scss";
+
+const MatchesProfiles = () => {
+  return (
+    <div className={styles.container}>
+      <div className={styles.matchesContainer}>
+        <div className={styles.matches}>
+          <img
+            className={styles.imgMatch}
+            src="https://images.unsplash.com/photo-1529333166437-7750a6dd5a70"
+            alt="friends"
+          />
+        </div>
+        <div className={styles.matches}>
+          <img
+            className={styles.imgMatch}
+            src="https://images.unsplash.com/photo-1529333166437-7750a6dd5a70"
+            alt="friends"
+          />
+        </div>
+        <div className={styles.matches}>
+          <img
+            className={styles.imgMatch}
+            src="https://images.unsplash.com/photo-1529333166437-7750a6dd5a70"
+            alt="friends"
+          />
+        </div>
+        <div className={styles.matches}>
+          <img
+            className={styles.imgMatch}
+            src="https://images.unsplash.com/photo-1529333166437-7750a6dd5a70"
+            alt="friends"
+          />
+        </div>
+        <div className={styles.matches}>
+          <img
+            className={styles.imgMatch}
+            src="https://images.unsplash.com/photo-1529333166437-7750a6dd5a70"
+            alt="friends"
+          />
+        </div>
+        <div className={styles.matches}>
+          <img
+            className={styles.imgMatch}
+            src="https://images.unsplash.com/photo-1529333166437-7750a6dd5a70"
+            alt="friends"
+          />
+        </div>
+        <div className={styles.matches}>
+          <img
+            className={styles.imgMatch}
+            src="https://images.unsplash.com/photo-1529333166437-7750a6dd5a70"
+            alt="friends"
+          />
+        </div>
+        <div className={styles.matches}>
+          <img
+            className={styles.imgMatch}
+            src="https://images.unsplash.com/photo-1529333166437-7750a6dd5a70"
+            alt="friends"
+          />
+        </div>
+        <div className={styles.matches}>
+          <img
+            className={styles.imgMatch}
+            src="https://images.unsplash.com/photo-1529333166437-7750a6dd5a70"
+            alt="friends"
+          />
+        </div>
+        <div className={styles.matches}>
+          <img
+            className={styles.imgMatch}
+            src="https://images.unsplash.com/photo-1529333166437-7750a6dd5a70"
+            alt="friends"
+          />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default MatchesProfiles;
