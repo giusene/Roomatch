ca0e5b84b81ec8490238a0167d10120c15671335
Rebecca Mollica <rebecca.mollica@live.it>
2022-02-17 10:10:12 +0100

    feat: add footer component (#103)
    * feat: add footer component
    
    * fix: responsive layout

--- src/Pages/LandingPage/index.jsx
+++ src/Pages/LandingPage/index.jsx
@@ -1,24 +1,27 @@
-import { Link } from "react-router-dom";
-import styles from "./LandingPage.module.scss";
-import homeHero from "./../../libs/img/homehero.jpeg";
+import { Link } from 'react-router-dom';
+import styles from './LandingPage.module.scss';
+import homeHero from './../../libs/img/homehero.jpeg';
+import Footer from './../../components/Footer/Footer'
 
 const LandingPage = () => {
   return (
-    <div
-      className={styles.main}
-      style={{ backgroundImage: `url(${homeHero})` }}
-    >
-      <div className={styles.mask}>
-      <div className={styles.title}>
-        <h1>FIND YOUR PERFECT ROOMMATE</h1>
-
-      </div>
-      <div className={styles.btnSet}>
-        <Link to={"/registration"}>Registrati</Link>
-        <Link to={"/login"}>Login</Link>
+    <>
+      <div
+        className={styles.main}
+        style={{ backgroundImage: `url(${homeHero})` }}
+      >
+        <div className={styles.mask}>
+          <div className={styles.title}>
+            <h1>FIND YOUR PERFECT ROOMMATE</h1>
           </div>
+          <div className={styles.btnSet}>
+            <Link to={'/registration'}>Registrati</Link>
+            <Link to={'/login'}>Login</Link>
+          </div>
+        </div>
       </div>
-    </div>
+      <Footer />
+    </>
   );
 };
 
---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,36 @@
+import styles from "./Footer.module.scss";
+
+const Footer = () => {
+  const year = new Date().getFullYear();
+
+  return (
+    <div className={styles.footerWrapper}>
+      <div className={styles.footer}>
+      <div className={styles.links}>
+
+        <ul>
+          <li>
+            <h2>Policy</h2>
+          </li>
+          <li>Community Policy</li>
+          <li>Privacy and cookies</li>
+        </ul>
+
+        <ul>
+          <li>
+            <h2>Contact us</h2>
+          </li>
+          <li>roomatch@info.com</li>
+          <li>Sede legale: via blablabla, 16 (CT) 95125</li>
+        </ul>
+      </div>
+      <hr />
+      <div className={styles.trademark}>
+        <p>ROOMATCH - ALL RIGHTS RESERVED ©  - {year}</p>
+      </div>
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
