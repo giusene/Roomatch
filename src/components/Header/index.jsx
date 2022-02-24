95d004a34ec3ce758a526881d4a10b7c1ba91eae
Stefano <io.stefanorusso@gmail.com>
2022-02-24 20:48:36 +0100

    fix: polling

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,26 +1,26 @@
 import styles from "./Header.module.scss";
 import TopNav from "./../TopNav";
 import { Link } from "react-router-dom";
-import { useEffect } from "react";
-import { useDispatch, useSelector } from "react-redux";
-import { updateAction } from "../../store/actions";
+// import { useEffect } from "react";
+// import { useDispatch, useSelector } from "react-redux";
+// import { updateAction } from "../../store/actions";
 
 const Header = () => {
-  const user = useSelector(state => state.user);
-  const dispatch = useDispatch();
+  // const user = useSelector((state) => state.user);
+  // const dispatch = useDispatch();
 
-  useEffect(() => {
-    const updateUser = setInterval(() => {
-      dispatch(
-        updateAction({
-          myId: user._id,
-          token: user.token,
-        })
-      );
-    }, 2000);
+  // useEffect(() => {
+  //   const updateUser = setInterval(() => {
+  //     dispatch(
+  //       updateAction({
+  //         myId: user._id,
+  //         token: user.token,
+  //       })
+  //     );
+  //   }, 2000);
 
-    return () => clearInterval(updateUser);
-  }, [dispatch, user._id, user.token]);
+  //   return () => clearInterval(updateUser);
+  // }, [dispatch, user._id, user.token]);
 
   return (
     <div className={styles.main}>
